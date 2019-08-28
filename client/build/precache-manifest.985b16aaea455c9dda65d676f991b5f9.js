97312563593fb38ea2ba45f25833876159b96feb
Hellon Canella <helloncanella@gmail.com>
2019-08-28 01:27:27 -0300

    fine

---
+++ client/build/precache-manifest.985b16aaea455c9dda65d676f991b5f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d70b4d0a75462d9aea449affa21aa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b7b527668a60996b75c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3378196c625885ed1388",
+    "url": "/static/js/2.07ce56c7.chunk.js"
+  },
+  {
+    "revision": "6b7b527668a60996b75c",
+    "url": "/static/js/main.f12226ce.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
